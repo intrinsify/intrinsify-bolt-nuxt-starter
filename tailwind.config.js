99ccb458d09ae919b5d6b582060b42c0d5ef641e
Nils Dannemann <ndannemann@gmail.com>
2024-11-06 16:25:00 +0100

    added primary colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,20 @@ export default {
       fontFamily: {
         sans: ['Barlow', 'sans-serif'],
       },
+      colors: {
+        primary: {
+          50: '#FEF9ED',  // Lightest
+          100: '#FCF3DB',
+          200: '#F9E4B7',
+          300: '#F6D594',
+          400: '#F4C771',
+          500: '#F2BA4F', // Your base color
+          600: '#C29540',
+          700: '#917030',
+          800: '#614C20',
+          900: '#302610',  // Darkest
+        }
+      }
     },
   },
   plugins: [],
